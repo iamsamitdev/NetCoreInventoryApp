75feb3c6596d29636009ea1b8f49e785d1c12a64
Samit Koyom <samitkoyom@gmail.com>
2023-12-21 16:41:56 +0700

    initial commit

---
+++ Controllers/BackendController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreInventory.Controllers
+{
+    public class BackendController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Forgotpassword()
+        {
+            return View();
+        }
+
+    }
+}
